6161b6647988ba4259a885f92776215ad3cdb612
safwan kanniyath <safwan@retailcloud.com>
2023-05-23 10:55:18 +0530

    changes in vm

--- src/app/base/base-api.ts
+++ src/app/base/base-api.ts
@@ -1,5 +1,5 @@
 export class BaseApi {
   public static isLive = true;
-  public static URL = BaseApi.isLive ? 'http://35.200.226.145/api': 'http://127.0.0.1:5000'
+  public static URL = BaseApi.isLive ? 'http://34.93.213.244/api': 'http://127.0.0.1:5000'
   public static PLATFORM_API_HOST_URL: string = BaseApi.URL;
 }
